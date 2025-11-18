061853f3ccfc9f7715907b677e376791a4faa2e9
EdwardDominguez <edyalexanderdommx@hotmail.com>
2025-11-18 02:28:50 -0600

    added again new category

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final categories = ["Ciencia ficción", "Drama", "Thriller"];
+    final categories = ["Ciencia ficción", "Drama", "Thriller", "Tecnología"];
 
     return Scaffold(
       appBar: AppBar(
