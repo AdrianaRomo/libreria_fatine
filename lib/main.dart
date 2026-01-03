8ac57b18bd03bfbb60b7428b496a8b5575800adf
AdrianaRomo <adrirs1809@gmail.com>
2026-01-03 02:58:36 -0600

    config ip y add search option

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _HomeScreenState extends State<HomeScreen> {
       final List<dynamic> data = jsonDecode(response.body);
       final books = data.map((e) => Book.fromJson(e)).toList();
 
-      // 🔥 AQUÍ SE LLENA allBooks
+      // AQUÍ SE LLENA allBooks
       allBooks = books;
 
       return books;
@@ -312,7 +312,7 @@ void _openAccountMenu(BuildContext context) async {
         builder: (_) => const LoginPage(fromCart: true),
       ),
     );
-    return; // ⛔ importante: no seguir
+    return; //  importante: no seguir
   }
 
   showModalBottomSheet(
